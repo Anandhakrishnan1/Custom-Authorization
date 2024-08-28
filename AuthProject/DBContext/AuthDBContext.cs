8d9166e73f19b7ad0ebba56fa5e7458f83b2e97d
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-08-28 12:13:19 +0530

    created DB context file

---
+++ AuthProject/DBContext/AuthDBContext.cs
@@ -0,0 +1,26 @@
+﻿using AuthProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthProject.DBContext
+{
+    public class AuthDBContext : IdentityDbContext<AppUser>
+    {
+        public AuthDBContext(DbContextOptions<AuthDBContext> options): base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>(
+                x =>
+                {
+                    x.ToTable("User");
+                });
+        }
+    }
+}
